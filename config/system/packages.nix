b73303e28cbcc224f2dcab26a11ac1eba7d35a30
princedimond <princedimond@gmail.com>
2025-02-03 22:25:46 -0500

    package updates
    update packages and create backup of the flake.lock file

--- config/system/packages.nix
+++ config/system/packages.nix
@@ -14,7 +14,7 @@
     toybox virt-viewer swappy ripgrep appimage-run 
     networkmanagerapplet yad playerctl nh gitkraken 
     github-desktop bitwarden ferdium expressvpn protonvpn-gui
-    orca-slicer helix helix-gpt
+    orca-slicer helix helix-gpt pkgs.gnome-disk-utility vlc
   ];
 
   programs = {
